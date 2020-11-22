729c71d03d3e29b694d5430da99b7ebc5a2d8697
LiuYu <likedust.thestars@gmail.com>
2020-11-22 14:01:38 +0800

    新增Boids算法

---
+++ Unity/Boids/Assets/Scripts/FlockBehaviors/AlignmentBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Alignment")]
+public class AlignmentBehavior : FlockBehavior
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (context.Count == 0)
+        {
+            return agent.transform.up;
+        }
+        Vector2 alignmentMove = Vector2.zero;
+        List<Transform> filterContext = filter == null ? context : filter.Filter(agent ,context);
+        foreach (Transform item in filterContext)
+        {
+            alignmentMove += (Vector2)item.transform.up;
+
+        }
+        alignmentMove /= context.Count;
+        return alignmentMove;
+    }
+}
---
+++ Unity/Boids/Assets/Scripts/FlockBehaviors/CohesionBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Cohesion")]
+public class CohesionBehavior : FlockBehavior
+{
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //if no neighbors, return no adjustment
+        if(context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+        //找到邻居的中点，尝试到这里去
+        Vector2 cohesionMove = Vector2.zero;
+        List<Transform> filterContext = filter == null ? context : filter.Filter(agent, context);
+        foreach (Transform item in filterContext)
+        {
+            cohesionMove += (Vector2)item.position;
+
+        }
+        cohesionMove /= context.Count;
+
+        //create offset 
+        cohesionMove -= (Vector2)agent.transform.position;
+        cohesionMove = Vector2.SmoothDamp(agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
+        return cohesionMove;
+    }
+}
